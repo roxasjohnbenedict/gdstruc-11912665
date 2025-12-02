37cf78f64a47be2e94c8813f28f4b4c145205a2b
Roxas, John Benedict <johnbenedict.roxas@benilde.edu.ph>
2025-12-02 17:25:49 +0800

    Finished Sample Project

--- Sample Project/Sample Project/src/Main.java
+++ Sample Project/Sample Project/src/Main.java
@@ -1,9 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    //IO.println(String.format("Hello and welcome!"));
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("What is your name?");
+    String name = scanner.nextLine();
+    
+    System.out.println("Hello " + name);
 
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
